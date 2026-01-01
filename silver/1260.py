15877a05b7020e3fdb03113c38c73a440d3c6831
javadocq <han18264@naver.com>
2026-01-01 16:07:47 +0900

    1260

---
+++ silver/1260.py
@@ -0,0 +1,100 @@
+# DFS와 BFS
+# 시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+# 2 초	128 MB	352015	143424	84539	39.263%
+# 문제
+# 그래프를 DFS로 탐색한 결과와 BFS로 탐색한 결과를 출력하는 프로그램을 작성하시오. 단, 방문할 수 있는 정점이 여러 개인 경우에는 정점 번호가 작은 것을 먼저 방문하고, 더 이상 방문할 수 있는 점이 없는 경우 종료한다. 정점 번호는 1번부터 N번까지이다.
+
+# 입력
+# 첫째 줄에 정점의 개수 N(1 ≤ N ≤ 1,000), 간선의 개수 M(1 ≤ M ≤ 10,000), 탐색을 시작할 정점의 번호 V가 주어진다. 다음 M개의 줄에는 간선이 연결하는 두 정점의 번호가 주어진다. 어떤 두 정점 사이에 여러 개의 간선이 있을 수 있다. 입력으로 주어지는 간선은 양방향이다.
+
+# 출력
+# 첫째 줄에 DFS를 수행한 결과를, 그 다음 줄에는 BFS를 수행한 결과를 출력한다. V부터 방문된 점을 순서대로 출력하면 된다.
+
+# 예제 입력 1 
+# 4 5 1
+# 1 2
+# 1 3
+# 1 4
+# 2 4
+# 3 4
+# 예제 출력 1 
+# 1 2 4 3
+# 1 2 3 4
+# 예제 입력 2 
+# 5 5 3
+# 5 4
+# 5 2
+# 1 2
+# 3 4
+# 3 1
+# 예제 출력 2 
+# 3 1 2 5 4
+# 3 1 4 2 5
+# 예제 입력 3 
+# 1000 1 1000
+# 999 1000
+# 예제 출력 3 
+# 1000 999
+# 1000 999
+
+import sys
+from collections import deque
+sys.setrecursionlimit(10**7)
+input = sys.stdin.readline
+
+n, m, r = map(int, input().split())
+visited = [False] * (n+1) # 방문 순서 제안
+cnt = 1
+
+def dfs(V, R):
+    global cnt # cnt를 글로벌하게 설정
+
+    visited[R] = True
+    print(R, end=" ")
+
+    if R not in V: # key값 중에 R이 없다면 return
+        return 
+
+    for v in V[R]: # 인접 정점들을 한번씩 순회
+        if visited[v] == False:
+            dfs(V, v)
+    
+def bfs(V, R):
+    global cnt # cnt를 글로벌하게 설정
+    q = deque()
+
+    visited[R] = True
+    q.append(R)
+    
+    while q:
+        cur = q.popleft()
+        print(cur, end=" ")
+
+        if cur not in V:
+            continue
+
+        for v in V[cur]:
+            if not visited[v]:
+                visited[v] = True
+                q.append(v)
+
+def solution():
+    dic = {}
+    global visited
+    for _ in range(m):
+        V, E = map(int, input().split())
+        dic.setdefault(V, []).append(E) # setdefault를 통해 값을 계속 추가
+        dic.setdefault(E, []).append(V) # 반대쪽도 추가
+
+    for k in dic:
+        dic[k].sort()
+
+    dfs(dic, r)
+
+    visited = [False] * (n+1) # 방문 순서 제안
+
+    print()
+    bfs(dic, r)
+
+solution()
+
