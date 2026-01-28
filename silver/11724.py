6ab538faf19d5f070429945821d6c7a545763188
javadocq <han18264@naver.com>
2026-01-28 19:30:00 +0900

    11724

---
+++ silver/11724.py
@@ -0,0 +1,63 @@
+# 연결 요소의 개수
+# 시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+# 3 초	512 MB	177920	81948	53348	42.757%
+# 문제
+# 방향 없는 그래프가 주어졌을 때, 연결 요소 (Connected Component)의 개수를 구하는 프로그램을 작성하시오.
+
+# 입력
+# 첫째 줄에 정점의 개수 N과 간선의 개수 M이 주어진다. (1 ≤ N ≤ 1,000, 0 ≤ M ≤ N×(N-1)/2) 둘째 줄부터 M개의 줄에 간선의 양 끝점 u와 v가 주어진다. (1 ≤ u, v ≤ N, u ≠ v) 같은 간선은 한 번만 주어진다.
+
+# 출력
+# 첫째 줄에 연결 요소의 개수를 출력한다.
+
+# 예제 입력 1 
+# 6 5
+# 1 2
+# 2 5
+# 5 1
+# 3 4
+# 4 6
+# 예제 출력 1 
+# 2
+# 예제 입력 2 
+# 6 8
+# 1 2
+# 2 5
+# 5 1
+# 3 4
+# 4 6
+# 5 4
+# 2 4
+# 2 3
+# 예제 출력 2 
+# 1
+
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**7)
+
+n,m = map(int, input().split())
+visited = [False] * (n+1)
+
+def dfs(v, graph_list):
+    visited[v] = True
+
+    for c in graph_list[v]:
+        if not visited[c]:
+            dfs(c, graph_list)
+
+graph_list = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    u, v = map(int, input().split())
+    graph_list[u].append(v)
+    graph_list[v].append(u)
+
+count = 0
+
+for i in range(1,n+1):
+    if not visited[i]:
+        dfs(i, graph_list)
+        count += 1
+
+print(count)
