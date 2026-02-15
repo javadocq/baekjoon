3477f4c5b252c49c20d3f678959e390bd70d74a9
javadocq <han18264@naver.com>
2026-02-15 18:46:39 +0900

    15666

---
+++ silver/15666.py
@@ -0,0 +1,70 @@
+# N과 M (12)
+# 시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+# 2 초	512 MB	29645	23404	20120	79.457%
+# 문제
+# N개의 자연수와 자연수 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
+
+# N개의 자연수 중에서 M개를 고른 수열
+# 같은 수를 여러 번 골라도 된다.
+# 고른 수열은 비내림차순이어야 한다.
+# 길이가 K인 수열 A가 A1 ≤ A2 ≤ ... ≤ AK-1 ≤ AK를 만족하면, 비내림차순이라고 한다.
+# 입력
+# 첫째 줄에 N과 M이 주어진다. (1 ≤ M ≤ N ≤ 8)
+
+# 둘째 줄에 N개의 수가 주어진다. 입력으로 주어지는 수는 10,000보다 작거나 같은 자연수이다.
+
+# 출력
+# 한 줄에 하나씩 문제의 조건을 만족하는 수열을 출력한다. 중복되는 수열을 여러 번 출력하면 안되며, 각 수열은 공백으로 구분해서 출력해야 한다.
+
+# 수열은 사전 순으로 증가하는 순서로 출력해야 한다.
+
+# 예제 입력 1 
+# 3 1
+# 4 4 2
+# 예제 출력 1 
+# 2
+# 4
+# 예제 입력 2 
+# 4 2
+# 9 7 9 1
+# 예제 출력 2 
+# 1 1
+# 1 7
+# 1 9
+# 7 7
+# 7 9
+# 9 9
+# 예제 입력 3 
+# 4 4
+# 1 1 2 2
+# 예제 출력 3 
+# 1 1 1 1
+# 1 1 1 2
+# 1 1 2 2
+# 1 2 2 2
+# 2 2 2 2
+
+import sys
+input = sys.stdin.readline
+
+def dfs(i, result, number_sequence, answer):
+    if len(result) == m:
+        if result not in answer:
+            print(*result)
+            answer.append(result)
+
+        return 
+    for start in range(i, n):
+        dfs(start, result + [number_sequence[start]], number_sequence, answer)
+
+def solution(number_sequence):
+    result = []
+    answer = []
+    number_sequence.sort()
+    
+    dfs(0, result, number_sequence, answer)
+
+n, m = map(int, input().split())
+number_sequence = list(map(int, input().split()))
+
+solution(number_sequence)
